05e8ee8588fbb2b91559ae587c720c9d4e3fef64
Jon Friesen <jfriesen@digitalocean.com>
2020-07-22 08:40:52 -0700

    restructure removing go funcs

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/jonfriesen/subscriber-tracker-worker
 go 1.14
 
 require (
-	github.com/Pallinder/go-randomdata v1.2.0 // indirect
+	github.com/Pallinder/go-randomdata v1.2.0
 	github.com/lib/pq v1.7.0
 )
--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"net/http/httputil"
 	"os"
 	"os/signal"
-	"sync"
 	"time"
 
 	randomdata "github.com/Pallinder/go-randomdata"
@@ -42,42 +41,34 @@ func main() {
 		}
 	}
 
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-
-	go func() {
-		sigquit := make(chan os.Signal, 1)
-		signal.Notify(sigquit, os.Interrupt, os.Kill)
-
-		sig := <-sigquit
-		log.Printf("caught sig: %+v", sig)
-		log.Printf("Gracefully shutting down server...")
-
-		wg.Done()
-	}()
-
-	go func() {
-
-		for {
-			newSub := &model.Subscriber{
-				Name:  randomdata.FullName(randomdata.RandomGender),
-				Email: randomdata.Email(),
-			}
-			newSubB, err := json.Marshal(newSub)
-			if err != nil {
-				log.Println("Error marshalling generated sub.")
-			}
-			hostPath := apiPath + "/subscribers/"
-
-			_, err = http.Post(hostPath, "application/json", bytes.NewBuffer(newSubB))
-			if err != nil {
-				log.Printf("Error occurred, ignoring: %s\n", err.Error())
-				time.Sleep(1 * time.Minute)
-			}
-			log.Printf("%s @ Added: %s <%s>", hostPath, newSub.Name, newSub.Email)
-			time.Sleep(1 * time.Second)
+	sigquit := make(chan os.Signal, 1)
+	signal.Notify(sigquit, os.Interrupt, os.Kill)
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-sigquit:
+			os.Exit(0)
+		case <-ticker.C:
 		}
-	}()
+		newSub := &model.Subscriber{
+			Name:  randomdata.FullName(randomdata.RandomGender),
+			Email: randomdata.Email(),
+		}
+		newSubB, err := json.Marshal(newSub)
+		if err != nil {
+			log.Println("Error marshalling generated sub.")
+			return
+		}
+		hostPath := apiPath + "/subscribers/"
 
-	wg.Wait()
+		resp, err := http.Post(hostPath, "application/json", bytes.NewBuffer(newSubB))
+		if err != nil {
+			log.Printf("Error occurred, ignoring: %s\n", err.Error())
+			time.Sleep(1 * time.Minute)
+			continue
+		}
+		defer resp.Body.Close()
+
+		log.Printf("%s @ Added: %s <%s>", hostPath, newSub.Name, newSub.Email)
+	}
 }
